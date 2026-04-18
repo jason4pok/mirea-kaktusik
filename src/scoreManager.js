0aea65972b7551a93251e1963355b436e67868e2
sergey <162305430+jason4pok@users.noreply.github.com>
2026-04-18 19:14:52 +0300

    Update scoreManager.js

--- src/scoreManager.js
+++ src/scoreManager.js
@@ -1,1 +1,23 @@
-// ScoreManager module - Mirea Kaktusik
+/**
+ * @fileoverview Модуль подсчёта очков и работы с рекордом.
+ * @module ScoreManager
+ */
+class ScoreManager {
+  /**
+   * @param {Object} storage - Сервис доступа к хранилищу.
+   */
+  constructor(storage) {
+    /** @type {number} */
+    this.currentScore = 0;
+    /** @type {number} */
+    this.bestScore = 0;
+  }
+  /** @param {number} points @returns {void} */
+  addPoints(points) {}
+  /** @returns {void} */
+  resetCurrent() {}
+  /** @returns {number} */
+  loadBest() {}
+  /** @returns {void} */
+  saveBest() {}
+}
