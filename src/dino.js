78d65dbd01c92b89021538226eec897c8a59ea37
sergey <162305430+jason4pok@users.noreply.github.com>
2026-04-18 19:14:22 +0300

    Update dino.js

--- src/dino.js
+++ src/dino.js
@@ -1,1 +1,19 @@
-// Dino module - Mirea Kaktusik
+/**
+ * @fileoverview Модуль персонажа-динозавра.
+ * @module Dino
+ */
+class Dino {
+  /**
+   * @param {number} jumpHeight - Высота прыжка в пикселях.
+   */
+  constructor(jumpHeight) {
+    /** @type {number} */
+    this.jumpHeight = jumpHeight;
+  }
+  /** @param {number} dt @returns {void} */
+  update(dt) {}
+  /** @returns {void} */
+  jump() {}
+  /** @returns {DOMRect} */
+  getBounds() {}
+}
