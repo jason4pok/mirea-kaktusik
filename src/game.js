181f3a4f3efae4ddae6b5667ed5acfe5eb3554d5
sergey <162305430+jason4pok@users.noreply.github.com>
2026-04-18 19:13:56 +0300

    Update game.js

--- src/game.js
+++ src/game.js
@@ -1,1 +1,26 @@
-// Game module - Mirea Kaktusik
+/**
+ * @fileoverview Центральный модуль управления игровой сессией.
+ * @module Game
+ */
+class Game {
+  /**
+   * @param {HTMLCanvasElement} canvas
+   * @param {Object} settings
+   */
+  constructor(canvas, settings) {
+    /** @type {string} */
+    this.state = 'idle';
+    /** @type {number} */
+    this.speed = 1.0;
+    /** @type {number} */
+    this.score = 0;
+  }
+  /** @returns {void} */
+  start() {}
+  /** @returns {void} */
+  pause() {}
+  /** @returns {void} */
+  reset() {}
+  /** @returns {boolean} */
+  gameOver() {}
+}
